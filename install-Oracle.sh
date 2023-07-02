0beea3b9f598e7c3df62a5725c9dd901b2a1ecef
Thepicolo <34195918+Thepicolo@users.noreply.github.com>
2023-07-02 19:06:07 +0200

    Actualizar install-Oracle.sh

--- install-Oracle.sh
+++ install-Oracle.sh
@@ -13,7 +13,7 @@ sudo alien -i oracle-instantclient-basic-21.10.0.0.0-1.el8.x86_64.rpm
 sudo alien -i oracle-instantclient-sqlplus-21.10.0.0.0-1.el8.x86_64.rpm
 sudo alien -i oracle-instantclient-devel-21.10.0.0.0-1.el8.x86_64.rpm
 
-sudo sh -c "echo /usr/lib/oracle/18.3/client64/lib > \
+sudo sh -c "echo /usr/lib/oracle/21/client64/lib > \
       /etc/ld.so.conf.d/oracle-instantclient.conf"
   sudo ldconfig
 
