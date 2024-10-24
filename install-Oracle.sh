ae0a04165b56eef0df927382349710633f5bc2b3
Thepicolo <34195918+Thepicolo@users.noreply.github.com>
2024-10-24 08:40:16 +0200

    Update install-Oracle.sh

--- install-Oracle.sh
+++ install-Oracle.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+# Actualización de paquetes e instalación de alien
 sudo apt-get update
 sudo apt-get install -y alien
 
-sudo cd $HOME
+# Cambio al directorio del usuario
+cd $HOME
 
+# Descargar los archivos RPM de Oracle Instant Client
 wget https://download.oracle.com/otn_software/linux/instantclient/2110000/oracle-instantclient-basic-21.10.0.0.0-1.el8.x86_64.rpm
 wget https://download.oracle.com/otn_software/linux/instantclient/2110000/oracle-instantclient-sqlplus-21.10.0.0.0-1.el8.x86_64.rpm
 wget https://download.oracle.com/otn_software/linux/instantclient/2110000/oracle-instantclient-devel-21.10.0.0.0-1.el8.x86_64.rpm
 
-sudo alien -i oracle-instantclient-basic-21.10.0.0.0-1.el8.x86_64.rpm
-sudo alien -i oracle-instantclient-sqlplus-21.10.0.0.0-1.el8.x86_64.rpm
-sudo alien -i oracle-instantclient-devel-21.10.0.0.0-1.el8.x86_64.rpm
+# Convertir RPM a DEB e instalar con dpkg
+sudo alien --to-deb oracle-instantclient-basic-21.10.0.0.0-1.el8.x86_64.rpm
+sudo alien --to-deb oracle-instantclient-sqlplus-21.10.0.0.0-1.el8.x86_64.rpm
+sudo alien --to-deb oracle-instantclient-devel-21.10.0.0.0-1.el8.x86_64.rpm
+sudo dpkg -i *.deb
 
-sudo sh -c "echo /usr/lib/oracle/21/client64/lib > \
-      /etc/ld.so.conf.d/oracle-instantclient.conf"
-  sudo ldconfig
+# Configurar Oracle Instant Client en ldconfig
+sudo sh -c "echo /usr/lib/oracle/21/client64/lib > /etc/ld.so.conf.d/oracle-instantclient.conf"
+sudo ldconfig
 
-sudo export LD_LIBRARY_PATH=/usr/lib/oracle/21/client64/lib:$LD_LIBRARY_PATH
+# Configurar las variables de entorno
+echo 'export LD_LIBRARY_PATH=/usr/lib/oracle/21/client64/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PATH=/usr/lib/oracle/21/client64/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 
+# Crear el directorio network/admin (opcional, verifica si es necesario)
 sudo mkdir -p /usr/lib/oracle/21/client64/lib/network/admin
 
-sudo export PATH=/usr/lib/oracle/21/client64/bin:$PATH
-
+# Instalar pip y cx-Oracle
 sudo apt -y install python3-pip
-pip install cx-Oracle
-
-rm oracle-instantclient-*.rpm
-echo "---AUTO-DESTROYING---"
-rm install-oracle.sh
+pip install --user cx-Oracle
